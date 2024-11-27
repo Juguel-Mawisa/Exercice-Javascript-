3f36a791cc80f398e300ed687d678c248fd58631
Juguel-Mawisa <juguelmawisa@gmail.com>
2024-11-28 00:59:39 +0100

    Vérifier si une tâche est terminé

---
+++ tache_terminée.js
@@ -0,0 +1,7 @@
+const courses = { title: "Faire les courses", status: "terminée" };
+
+if (courses.status === "terminée") {
+    console.log("La tâche est terminée !");
+} else {
+    console.log("La tâche n'est pas encore terminée.");
+}
