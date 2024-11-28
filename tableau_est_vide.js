7e2c1a79ef328fa6c3363cd3aa1bd4a9243de8c5
Juguel-Mawisa <juguelmawisa@gmail.com>
2024-11-28 03:50:42 +0100

    Vérifier si un tableau est vide

---
+++ tableau_est_vide.js
@@ -0,0 +1,9 @@
+        const tableau = []; 
+
+        const element = document.getElementById("obed");
+
+        if (!tableau.length) {
+            element.innerHTML = "Le tableau est vide.";
+        } else {
+            element.innerHTML = "Le tableau n'est pas vide.";
+        }
