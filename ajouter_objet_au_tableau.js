c1b66e860ff62929426cdfbad2cb39c421cbb1fa
Juguel-Mawisa <juguelmawisa@gmail.com>
2024-11-29 19:08:14 +0100

    Ajouter une tâche à une liste

--- ajouter_objet_au_tableau.js
+++ ajouter_objet_au_tableau.js
@@ -7,33 +7,37 @@ const inputDescription = document.getElementById("description");
 const selectStatus = document.getElementById("status");
 
 function afficherTaches() {
-
   conteneurTaches.innerHTML = "";
 
-  etudiant.forEach((tache, index) => {
+  etudiant.forEach((tache) => {
     conteneurTaches.innerHTML += `
       <div class="tache">
         <p><strong>${tache.description}</strong> (${tache.status})</p>
-        
       </div>
     `;
   });
 }
 
-
 formTache.addEventListener("submit", (e) => {
   e.preventDefault(); 
 
-  const tache = {
+  const nouvelleTache = {
     id: etudiant.length + 1,
-    description: inputDescription.value,
+    description: inputDescription.value.trim(),
     status: selectStatus.value,
   };
 
-  etudiant.push(tache);
+  const existeDeja = etudiant.some(
+    (tache) => tache.description === nouvelleTache.description && tache.status === nouvelleTache.status
+  );
 
-  afficherTaches();
-
-  formTache.reset();
+  if (existeDeja) {
+    alert("Cette tâche existe déjà !");
+  } else {
+    etudiant.push(nouvelleTache);
+    afficherTaches();
+    formTache.reset();
+  }
 });
+
 
